94ac5eed93c2fc5a314952c6f450b96ed0e72571
Marco Brandizi <marco.brandizi@gmail.com>
2017-10-05 14:53:46 +0100

    Initial commit.

---
+++ src/test/java/info/marcobrandizi/learn/lucene/BasicLuceneTest.java
@@ -0,0 +1,117 @@
+package info.marcobrandizi.learn.lucene;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: comment me!
+ *
+ * @author brandizi
+ * <dl><dt>Date:</dt><dd>31 Aug 2017</dd></dl>
+ *
+ */
+public class BasicLuceneTest
+{
+	private Logger log = LoggerFactory.getLogger ( this.getClass () );
+	
+	@Test
+	public void testBasics () throws Exception
+	{
+		// Create
+		Directory index = new RAMDirectory ();
+		StandardAnalyzer analyzer = new StandardAnalyzer ();
+		IndexWriterConfig cfg = new IndexWriterConfig ( analyzer );
+		IndexWriter w = new IndexWriter ( index, cfg );
+
+		addDoc ( w, "Just a Test", "doc1" );
+		addDoc ( w, "Another Test", "doc2" );
+		addDoc ( w, "Yet Another test", "doc3" );	
+		addDoc ( w, "My Personal Doc", "doc4" );	
+		
+		w.close ();
+		
+		
+		// Search
+		IndexReader idxRdr = DirectoryReader.open ( index );
+		IndexSearcher searcher = new IndexSearcher ( idxRdr );	
+		
+		log.info ( "Search 1" );
+		ScoreDoc[] scoreDocs = searchByTitle ( searcher, analyzer, "test" );
+		logResults ( searcher, scoreDocs );
+		Assert.assertEquals ( "Wrong no. of results", 3, scoreDocs.length );
+		
+		log.info ( "Search 2" );
+		scoreDocs = searchByTitle ( searcher, analyzer, "personal" );
+		logResults ( searcher, scoreDocs );
+		Assert.assertEquals ( "Wrong no. of results", 1, scoreDocs.length );
+
+		log.info ( "Search 3" );
+		scoreDocs = searchAllFields ( searcher, analyzer, "title:\"just a test\" OR docId:\"DOC2\"" );
+		logResults ( searcher, scoreDocs );
+		Assert.assertEquals ( "Wrong no. of results", 2, scoreDocs.length );
+	}
+	
+
+	private void addDoc ( IndexWriter w, String keyword, String id ) throws IOException
+	{
+		Document doc = new Document ();
+		doc.add ( new TextField ( "title", keyword, Store.YES ) );
+		doc.add ( new StringField ( "docId", id, Store.YES ) );
+		w.addDocument ( doc );
+	}
+	
+	
+	private ScoreDoc[] search ( IndexSearcher searcher, Supplier<QueryParser> queryParserSupplier, String queryStr ) 
+		throws ParseException, IOException
+	{
+		Query q = queryParserSupplier.get ().parse ( queryStr );
+		TopDocs topDocs = searcher.search ( q, 10 );
+		return topDocs.scoreDocs;
+	}
+
+	private ScoreDoc[] searchByTitle ( IndexSearcher searcher, final Analyzer analyzer, String queryStr ) 
+		throws ParseException, IOException
+	{
+		return search ( searcher, () -> new QueryParser ( "title", analyzer ), queryStr );
+	}
+	
+	private ScoreDoc[] searchAllFields ( IndexSearcher searcher, Analyzer analyzer, String queryStr ) 
+		throws ParseException, IOException
+	{
+		return search ( searcher, () -> new MultiFieldQueryParser ( new String [] { "title", "docId" }, analyzer ), queryStr );
+	}
+	
+	
+	private void logResults ( IndexSearcher searcher, ScoreDoc[] scoreDocs ) throws IOException
+	{
+		for ( ScoreDoc scoreDoc: scoreDocs )
+		{
+			Document doc = searcher.doc ( scoreDoc.doc );
+			log.info ( "Document found: '{}', '{}', score: {}", doc.get ( "title" ), doc.get ( "docId" ), scoreDoc.score );
+		}
+	}
+}
---
+++ src/test/java/info/marcobrandizi/learn/lucene/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TODO: comment me!
+ *
+ * @author brandizi
+ * <dl><dt>Date:</dt><dd>31 Aug 2017</dd></dl>
+ *
+ */
+package info.marcobrandizi.learn.lucene;
